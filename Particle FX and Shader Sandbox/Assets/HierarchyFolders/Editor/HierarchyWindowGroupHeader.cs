f69c277db4d9591521f0d51a4c51bacff4ded988
Ray Soyama <raysoyama@gmail.com>
2019-07-13 22:19:28 -0700

    Added new scripts, scenes, prefabs, and reorginized shit

---
+++ Particle FX and Shader Sandbox/Assets/HierarchyFolders/Editor/HierarchyWindowGroupHeader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class HierarchyWindowGroupHeader
+{
+    static HierarchyWindowGroupHeader()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+    }
+
+    static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    {
+        var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+        if (gameObject != null && gameObject.name.StartsWith("---", System.StringComparison.Ordinal))
+        {
+            EditorGUI.DrawRect(selectionRect, new Color(0.2f, 0.5f, 0.5f, 1));
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("-", "").ToUpperInvariant());
+        }
+        else
+        if (gameObject != null && gameObject.name.StartsWith(">>>", System.StringComparison.Ordinal))
+        {
+            EditorGUI.DrawRect(selectionRect, new Color(0.7f, 0.2f, 0.2f, 1));
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace(">", "").ToUpperInvariant());
+        }
+        else
+        if (gameObject != null && gameObject.name.StartsWith("!!!", System.StringComparison.Ordinal))
+        {
+            EditorGUI.DrawRect(selectionRect, new Color(0.3f, 0.4f, 0.5f, 1));
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("!", "").ToUpperInvariant());
+        }
+        else
+        if (gameObject != null && gameObject.name.StartsWith("^^^", System.StringComparison.Ordinal))
+        {
+            EditorGUI.DrawRect(selectionRect, new Color(0.6f, 0.6f, 0.3f, 1));
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("^", "").ToUpperInvariant());
+        }
+    }
+}
---
+++ Particle FX and Shader Sandbox/Assets/_SterlingFX/Script/Rays Scripts/FXShowcaseScript.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXShowcaseScript : MonoBehaviour
+{
+    public enum ShowCaseType
+    {
+        shoot,
+        follow,
+        impact
+    }
+
+    public enum Element
+    {
+        flame,
+        ice,
+        shock
+    }
+
+    private ShowCaseType previousMode;
+    public ShowCaseType currentMode;
+
+    private Element previousElement;
+    public Element currentElement;
+
+
+    public float bulletSpeed;
+
+    [Header("Camera Parameters")]
+    public Vector3 followCameraOffset;
+
+    [Header("Bullet Muzzle Flash")]
+    public GameObject FlameMuzzleFlashPrefab;
+    public GameObject IceMuzzleFlashPrefab;
+    public GameObject ShockMuzzleFlashPrefab;
+
+    [Header("Bullet Prefabs")]
+    public GameObject FlameBulletPrefab;
+    public GameObject IceBulletPrefab;
+    public GameObject ShockBulletPrefab;
+
+    [Header("Bullet Impact Prefabs")]
+    public GameObject FlameImpactPrefab;
+    public GameObject IceImpactPrefab;
+    public GameObject ShockImpactPrefab;
+
+
+
+    [ReadOnlyField]
+    public Camera mainCamera;
+    [ReadOnlyField]
+    public GameObject BulletInstance;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        previousMode = currentMode;
+        previousElement = currentElement;
+
+        InitializeDemo();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentMode != previousMode)
+        {
+            Debug.Log("Mode Changed");
+        }
+
+        if (currentElement != previousElement)
+        {
+            Destroy(BulletInstance);
+            InitializeDemo();
+            previousElement = currentElement;
+        }
+
+        switch (currentMode)
+        {
+            case ShowCaseType.follow:
+                mainCamera.transform.position = BulletInstance.transform.position + followCameraOffset;
+                break;
+        }
+    }
+
+
+
+    private GameObject InstantiateBullet()
+    {
+        switch (currentElement)
+        {
+            case Element.flame:
+                BulletInstance = Instantiate(FlameBulletPrefab);
+                break;
+
+            case Element.ice:
+                BulletInstance = Instantiate(IceBulletPrefab);
+                break;
+
+            case Element.shock:
+                BulletInstance = Instantiate(ShockBulletPrefab);
+                
+                break;
+        }
+
+        return BulletInstance;
+    }
+
+    private void InitializeDemo()
+    {
+        switch (currentMode)
+        {
+            case ShowCaseType.follow:
+                InstantiateBullet().GetComponent<Rigidbody>().AddForce(Vector3.forward * bulletSpeed);
+                mainCamera.transform.position = BulletInstance.transform.position + followCameraOffset;
+                break;
+
+        }
+    }
+
+}
