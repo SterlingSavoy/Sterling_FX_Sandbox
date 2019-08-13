662a85f68b33fd4bfec9a2c70c97fa62a9d90db5
RedSSavoy <52873695+RedSSavoy@users.noreply.github.com>
2019-08-12 22:57:23 -0700

    FX scene changes
    If I fuck shit up sry

---
+++ Particle FX and Shader Sandbox/Assets/_SterlingFX/Script/animController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animController : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.Play("Projectile_01");
+        }
+    }
+}
